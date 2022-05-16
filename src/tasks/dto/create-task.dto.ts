2c7f136eee29a3cf29b908bcdb50e2d3f451eef2
JMustang <eecfredes@gmail.com>
2022-05-16 20:32:28 -0300

    Error Handling

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,10 @@
+// yarn add class-validator class-transformer
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
